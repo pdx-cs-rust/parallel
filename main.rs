871d0a40224bc70a1a37dc8cabf75202a4486529
Bart Massey <bart@cs.pdx.edu>
2018-05-22 15:48:10 -0700

    got sequential demo working

--- main.rs
+++ main.rs
@@ -1,6 +1,28 @@
 mod randstat;
 use randstat::randstats;
 
+extern crate rand;
+use self::rand::random;
+
+const BLOCKSIZE: usize = 10 * 1024 * 1024;
+
+fn make_rands(n: usize) -> Vec<Vec<f64>> {
+    let mut result = Vec::new();
+    for _ in 0..n {
+        let block: Vec<f64> =
+            (0..BLOCKSIZE).map(|_| random() ).collect();
+        result.push(block);
+    }
+    result
+}
+
+fn sequential(n: usize) {
+    let rands = make_rands(n);
+    for i in 0..n {
+        println!("{:?}", randstats(&rands[i]));
+    }
+}
+
 fn main() {
-    println!("{:?}", randstats());
+    sequential(8);
 }
--- randstat.rs
+++ randstat.rs
@@ -1,19 +1,12 @@
-extern crate rand;
-use self::rand::random;
-
-const BLOCKSIZE: usize = 10 * 1024 * 1024;
-
-pub fn randstats() -> (f64, f64) {
-    let randblock: Vec<f64> =
-        (0..BLOCKSIZE).map(|_| random() ).collect();
+pub fn randstats(randblock: &[f64]) -> (f64, f64) {
     let mut mean = 0.0;
-    for v in &randblock {
+    for v in randblock {
         mean += *v;
     }
-    let blocksize = BLOCKSIZE as f64;
+    let blocksize = randblock.len() as f64;
     mean /= blocksize;
     let mut var = 0.0;
-    for v in &randblock {
+    for v in randblock {
         let diff = *v - mean;
         var += diff * diff;
     }
