bd34c3df3c280075ede7cac529b8c9d2bef45a78
Bart Massey <bart@cs.pdx.edu>
2018-05-22 16:07:23 -0700

    added readme and license

---
+++ README.md
@@ -0,0 +1,9 @@
+# parallel
+Copyright (c) 2018 Bart Massey
+
+Rust parallel programming demos inspired by the Blandy \&
+Orendorff chapter.
+
+This program is licensed under the "MIT License".  Please
+see the file LICENSE in the source distribution of this
+software for license terms.
--- randstat.rs
+++ randstat.rs
@@ -1,3 +1,8 @@
+// Copyright © 2018 Bart Massey
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
 pub fn randstats(randblock: &[f64]) -> (f64, f64) {
     let mut mean = 0.0;
     for v in randblock {
